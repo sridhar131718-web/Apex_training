56b151422f815fb3de0ce1be34e1349da0839d2e
Sridhar A <sridhar131718@gmail.com>
2026-09-23 11:37:43 +0530

    Python Operators
    Python Operators Programs

---
+++ DAY-03/Logical Opeartors.py
@@ -0,0 +1,8 @@
+x=5
+print(x>3 and x<10)
+
+x=5
+print(x>3 or x<4)
+
+x=5
+print(not(x>3 and x<10))
---
+++ DAY-03/Membership Operators.py
@@ -0,0 +1,5 @@
+x=["apple","banana"]
+print("banana"in x)
+
+x=["apple","banana"]
+print("pineapple"not in x)
