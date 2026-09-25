ea331aadb6fd728f8a1832eca6cd84a8a332d6f5
Sridhar A <sridhar131718@gmail.com>
2026-09-25 09:54:07 +0530

    looping control statements
    looping control statements program

---
+++ DAY-05/looping control statements.py
@@ -0,0 +1,17 @@
+for i in range(1,6):
+    if i==3:
+        break
+    print(i)
+
+for i in range(1,7):
+    if i==4:
+        continue
+    print(i)
+
+x=10
+
+if x>5:
+    pass
+else:
+    print("Less than 5")
+print("Done")
